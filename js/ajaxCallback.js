8a4fa61d4c97f93ac29995f0b4d0ecae80248b04
usha-12 <usha12ahirwar@gmail.com>
2023-03-08 17:25:33 +0530

    Added: Create a simple html file to test the REST services with JSON Server

--- js/ajaxCallback.js
+++ js/ajaxCallback.js
@@ -1,40 +1,67 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-function makePromiseCall(methodType, url, async = true, data = null){
-    return new Promise(function (resolve, reject){
-        let chr = new XMLHttpRequest();
-        XPathResult.onreadystatechange = function(){
-            console.log("State Changed called. ready state: "+ XPathResult.readyState+"Status:"+xhr.status);
-            if(xhr.status.toString().match('^[2] [0-9] {2}$')){
-                resolve(xhr.responseTest);
-            }else if (xhr.status.toString().match('^[4,5] [0-9] {2}$')){
-                reject({
-                    status: xhr.status,
-                    statusText: xhr.statusText 
-                });
-                console.log("XHR Failed");
-            }
-        }
-        xhr.open(methodType, url, async);
-        if(data){
-            console.log(JSON.stringify(data));
-            xhr.setRequestheader("Content-Type", "application/json");
-            xhr.send(JSON.stringify(data));
-        }else xhr.send();
-        console.log(methodType+"request sent to the server");
-        
-    });
+function showTime(){
+    const date = new Date();
+    return date.getHours + " Hrs, " + date.getMinutes + " Min " + date.getSeconds() + "Secs";
 }
-const getURL = "http://127.0.0.1:3000/employees/1";
-makePromiseCall("GET", getURL, true)
-.then(responseTest => {
-    console.log("Get user Data: "+responseTest)
-})
-.catch(error => console.log("get error Data : "+JSON.stringify(error)));
-const postURL = "http://localhost:3000/employees";
-const emplData = {"Name": "Harry", "salary": "5000"};
-makePromiseCall("POST", postURL, true, emplData)
-.then(responseTest => {
-    console.log("User Added: "+responseTest)
-})
-.catch(error => console.log("POST Error Status: "+JSON.stringify(error)));
 
+//callback
+function makeAJAXCall(methodType, url, callback, async = true, data = null){
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);            
+        } else if (xhr.status >= 400){
+            console.log("Handle 400 client Error or 500 server Error");
+        }
+    }
+    xhr.open(methodType, url, async);
+    xhr.send();
+    console.log(methodType+" request send to server at "+showTime());
+}
+//promise
+function promiseCall(methodType, url, callback, async = true, data = null){
+    return new Promise(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    // xhr.onreadystatechange = function()
+    xhr.onload = function(){
+        console.log(methodType+" State Change Called. Ready State: "+ xhr.onreadystatechange+" Status: "+xhr.status);
+        if (xhr.readyState == 4) {
+            resolve(xhr.responseText);            
+        } else if (xhr.status >= 400){
+            reject({
+                status : xhr.status,
+                statusText : xhr.statusText
+            });
+            console.log("Handle 400 client Error or 500 server Error");
+        }
+    }
+    xhr.onerror = function() {
+        reject({
+            status : this.status,
+            statusText : xhttp.statusText
+        });
+    };
+    xhr.open(methodType, url, async);
+    if(data) {
+        console.log(JSON.stringify(data));
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(JSON.stringify(data));
+    }
+    else {
+        xhr.send();
+    }
+    console.log(methodType+" request send to server at "+showTime());
+});
+}
+// const getURL = "http://localhost:3000/employees/1";
+// function getUserDetails(data){
+//     console.log("Get User Data at : " + showTime() + " data: " + data)
+// }
+// makeAJAXCall("GET", getURL, getUserDetails, true);
+
+// promiseCall("GET", getURL, true)
+//     .then(responseText => {
+//     console.log("Get User Data: "+responseText)
+// })
+// .catch(error => console.log("GET Error Status : "+JSON.stringify(error)));
